4c3d0aa34a2152fb264c32d917afd8aa246afd3c
Alex Williams <alex@jump24.co.uk>
2021-01-04 13:25:35 +0000

    NEW: Add meta tag to app.blade.php template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
     <title>Marvel Tester - @yield('title')</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @if (isset($meta))
+        <meta name="description" content="{{ $meta }}">
+    @endif
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
 
