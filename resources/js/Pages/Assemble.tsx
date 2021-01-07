57835a0f437a24fc4751310d1a4232eb510e9145
James Seconde <jim@jump24.co.uk>
2021-01-07 15:02:38 +0000

    Update: move type to it's own definition file

--- resources/js/Pages/Assemble.tsx
+++ resources/js/Pages/Assemble.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Layout from '../Layout'
-import { CharacterType } from '../types/character'
 import CharacterList from '../Components/CharacterList'
-
-type CharactersProps = {
-  characters: Record<'data', Array<CharacterType>>
-}
+import { CharactersProps } from '../types/characterProps'
 
 const Characters = (props: CharactersProps): JSX.Element => {
   return <CharacterList characters={props.characters.data} />
