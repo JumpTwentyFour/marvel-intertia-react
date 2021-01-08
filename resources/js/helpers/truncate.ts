6c33ebb79938e240b94536a05c614cc190fdaad3
Anneka Mistry <anneka@jump24.co.uk>
2021-01-08 14:36:03 +0000

    UPDATE: truncate helper

---
+++ resources/js/helpers/truncate.ts
@@ -0,0 +1,9 @@
+export const truncate = (field: string, maxChar: number): string => {
+  if (field.length > maxChar) {
+    field = field.substring(0, maxChar)
+    field = field.substring(0,
+      Math.min(field.length, field.lastIndexOf(" "))) + " . . ."
+    return field
+  }
+  return field
+}
