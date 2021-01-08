af28bda0fa277e299fcfc6d14aee940a8f37a5d1
Alex Williams <alex@jump24.co.uk>
2021-01-08 07:59:26 +0000

    UPDATE: Fix test

--- tests/Feature/AssembleTest.php
+++ tests/Feature/AssembleTest.php
@@ -15,5 +15,13 @@ class AssembleTest extends TestCase
         $response->assertStatus(Response::HTTP_OK);
         $response->assertHasProp('characters');
         $response->assertPropCount('characters.data', 7);
+
+        $response->assertPropValue('characters.data.0.name', 'Captain America');
+        $response->assertPropValue('characters.data.1.name', 'Iron Man');
+        $response->assertPropValue('characters.data.2.name', 'Thor');
+        $response->assertPropValue('characters.data.3.name', 'Hulk');
+        $response->assertPropValue('characters.data.4.name', 'Wasp');
+        $response->assertPropValue('characters.data.5.name', 'Hank Pym');
+        $response->assertPropValue('characters.data.6.name', 'Black Widow');
     }
 }
