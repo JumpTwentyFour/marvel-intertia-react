5ff562e6d10e0061d231d629dc36b1eb01fce591
Anneka Mistry <anneka@jump24.co.uk>
2021-01-08 10:29:04 +0000

    UPDATE: linting issue

--- resources/js/Components/CharacterCard.tsx
+++ resources/js/Components/CharacterCard.tsx
@@ -6,7 +6,11 @@ const CharacterCard = (props: CharacterType): JSX.Element => {
   return (
     <div className='card col-span-6 sm:col-span-3 md:col-span-4 bg-blue shadow-4xl hover:shadow-3xl flex flex-col'>
       <div className='card__image w-full relative overflow-hidden flex items-center content-center'>
-        <img src={characterImageUrl} alt={props.name} className='w-full absolute object-cover' />
+        <img
+          src={characterImageUrl}
+          alt={props.name}
+          className='w-full absolute object-cover'
+        />
       </div>
       <div className='card__details flex flex-col p-5 md:p-8 lg:p-10'>
         <h3 className='card__title text-3xl font-semibold'>{props.name}</h3>
