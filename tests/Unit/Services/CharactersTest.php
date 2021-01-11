797807c907efb555d6ce9ac55856225ca7f65217
Alex Williams <alex@jump24.co.uk>
2021-01-11 13:48:03 +0000

    UPDATE: Add test

---
+++ tests/Unit/Services/CharactersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Services\Characters;
+use Tests\TestCase;
+
+class CharactersTest extends TestCase
+{
+    public function test_will_return_list_of_six_characters_for_homepage(): void
+    {
+        $characters = $this->app->make(Characters::class);
+
+        $randomCharacters = $characters->randomCharactersForHomepage();
+
+        $this->assertCount(6, $randomCharacters);
+    }
+}
