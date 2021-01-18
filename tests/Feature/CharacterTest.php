626d09c2b2076325f972308272d68f83f567c699
Alex Williams <alex@jump24.co.uk>
2021-01-18 20:44:44 +0000

    NEW: Hookup inertia testing library

--- tests/Feature/CharacterTest.php
+++ tests/Feature/CharacterTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use ClaudioDekker\Inertia\Assert;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
@@ -9,10 +10,16 @@ class CharacterTest extends TestCase
 {
     public function test_will_return_characters_by_name(): void
     {
-        $this->get('/characters?name=Thor Girl')
-            ->assertStatus(Response::HTTP_OK)
-            ->assertHasProp('characters')
-            ->assertPropCount('characters.data', 1)
-            ->assertPropValue('characters.data.0.name', 'Thor Girl');
+        $response = $this->get('/characters?name=Thor Girl');
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertInertia(fn (Assert $inertia) => $inertia
+            ->component('Characters', false)
+            ->has('characters.data', 1)
+            ->has('characters.data', fn (Assert $inertia) => $inertia
+                ->where('0.name', 'Thor Girl')
+                ->etc()
+            )
+            ->has('errors')
+        );
     }
 }
--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use ClaudioDekker\Inertia\Assert;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
@@ -9,50 +10,45 @@ class HomeTest extends TestCase
 {
     public function test_homepage_will_return_six_random_characters(): void
     {
-        $this->get('/')
-            ->assertStatus(Response::HTTP_OK)
-            ->assertHasProp('characters')
-            ->assertPropCount('characters.data', 6)
-            ->assertPropValue('characters.data', function ($characters) {
-                $this->assertEquals(
-                    [
-                        'id',
-                        'name',
-                        'description',
-                        'thumbnail',
-                        'resourceURI',
-                        'comics',
-                        'series',
-                        'stories',
-                        'events',
-                        'urls',
-                    ],
-                    array_keys($characters[0])
-                );
-
-                $this->assertEquals(['path', 'extension'], array_keys($characters[0]['thumbnail']));
-
-                $this->assertEquals(
-                    ['available', 'collectionURI', 'items', 'returned'],
-                    array_keys($characters[0]['comics'])
-                );
-
-                $this->assertEquals(
-                    ['available', 'collectionURI', 'items', 'returned'],
-                    array_keys($characters[0]['series'])
-                );
-
-                $this->assertEquals(
-                    ['available', 'collectionURI', 'items', 'returned'],
-                    array_keys($characters[0]['stories'])
-                );
-
-                $this->assertEquals(
-                    ['available', 'collectionURI', 'items', 'returned'],
-                    array_keys($characters[0]['events'])
-                );
-
-                $this->assertEquals(['type', 'url'], array_keys($characters[0]['urls'][0]));
-            });
+        $response = $this->get('/');
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertInertia(fn (Assert $inertia) => $inertia
+            ->component('Home', false)
+            ->has('characters.data', 6, fn (Assert $inertia) => $inertia
+                ->hasAll([
+                    'id',
+                    'name',
+                    'description',
+                    'thumbnail',
+                    'thumbnail.path',
+                    'thumbnail.extension',
+                    'resourceURI',
+                    'comics',
+                    'comics',
+                    'comics.available',
+                    'comics.collectionURI',
+                    'comics.items',
+                    'comics.returned',
+                    'series',
+                    'series.available',
+                    'series.collectionURI',
+                    'series.items',
+                    'series.returned',
+                    'stories',
+                    'stories.available',
+                    'stories.collectionURI',
+                    'stories.items',
+                    'stories.returned',
+                    'events',
+                    'events.available',
+                    'events.collectionURI',
+                    'events.items',
+                    'events.returned',
+                    'urls',
+                    'urls.0.type',
+                    'urls.0.url',
+                ])
+            )
+        );
     }
 }
