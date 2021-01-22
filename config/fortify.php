bd5485fed3212de9e901285cf649780f1cd86feb
Alex Williams <alex@jump24.co.uk>
2021-01-22 09:30:03 +0000

    REMOVE: Code comments

--- config/fortify.php
+++ config/fortify.php
@@ -134,12 +134,6 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-//        Features::emailVerification(),
-//        Features::updateProfileInformation(),
-//        Features::updatePasswords(),
-//        Features::twoFactorAuthentication([
-//            'confirmPassword' => true,
-//        ]),
     ],
 
 ];
