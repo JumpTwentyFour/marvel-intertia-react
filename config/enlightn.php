b58f1e176621b4d1b3da789df855262157cf6808
Alex Williams <alex@jump24.co.uk>
2021-01-25 09:42:56 +0000

    UPDATE: Use j24 pakage

--- config/enlightn.php
+++ config/enlightn.php
@@ -35,7 +35,7 @@ return [
     'analyzer_paths' => [
         'Enlightn\\Enlightn\\Analyzers' => base_path('vendor/enlightn/enlightn/src/Analyzers'),
         'Enlightn\\EnlightnPro\\Analyzers' => base_path('vendor/enlightn/enlightnpro/src/Analyzers'),
-        'App\\Enlightn\\Analyzers' => base_path('app/Enlightn//Analyzers'),
+        'JumpTwentyFour\\ProjectAnalyzers\\Analyzers' => base_path('vendor/jumptwentyfour/project-analysers/src/Analyzers')
     ],
 
     /*
