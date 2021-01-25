7380c8ed207d41feeee233a2853fe928ea75ecba
Alex Williams <alex@jump24.co.uk>
2021-01-25 13:53:57 +0000

    NEW: Reset password

---
+++ resources/js/Components/Form/Status.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+
+type StatusType = {
+  value: string
+}
+
+const Status = (props: StatusType): ReactElement => {
+  return (
+    <div className='px-6 py-4 border-0 rounded relative mb-4 bg-green-600'>
+      <span className='inline-block align-middle mr-8'>{props.value}</span>
+    </div>
+  )
+}
+
+export default Status
