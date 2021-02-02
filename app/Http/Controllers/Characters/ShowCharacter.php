6630a6a89e4bd2d1fb66df0a957932baf98bad23
Alex Williams <alex@jump24.co.uk>
2021-02-02 11:44:51 +0000

    UPDATE: Fix cs issue

--- app/Http/Controllers/Characters/ShowCharacter.php
+++ app/Http/Controllers/Characters/ShowCharacter.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Characters;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Character;
+use App\Services\Characters;
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Services\Characters;
 
 class ShowCharacter extends Controller
 {
