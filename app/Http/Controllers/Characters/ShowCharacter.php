61f8901a208c32a0c2f047949fe1c4a42fe70346
Alex Williams <alex@jump24.co.uk>
2021-02-03 15:49:21 +0000

    UPDATE: Tidy

--- app/Http/Controllers/Characters/ShowCharacter.php
+++ app/Http/Controllers/Characters/ShowCharacter.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Character;
 use App\Http\Resources\Comics;
 use App\Services\Characters;
+use Illuminate\Http\Response as LaravelResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -16,7 +17,7 @@ class ShowCharacter extends Controller
         $character = $characterClient->getCharacter($characterId);
 
         if (!isset($character['id'])) {
-            abort(404);
+            abort(LaravelResponse::HTTP_NOT_FOUND);
         }
 
         $comics = $characterClient->getComicsForCharacter($characterId);
