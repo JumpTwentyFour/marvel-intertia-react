7f0ab9c2c61756d0cbf301a81d7fee997a1b53ee
Daniel Newns <dan@jump24.co.uk>
2021-02-07 21:26:02 +0000

    update: adding link to the image

--- resources/js/Components/CharacterCard.tsx
+++ resources/js/Components/CharacterCard.tsx
@@ -13,11 +13,16 @@ const CharacterCard = (props: CharacterType): JSX.Element => {
       className='card col-span-6 sm:col-span-3 md:col-span-4 bg-blue shadow-4xl hover:shadow-3xl flex flex-col'
     >
       <div className='card__image w-full relative overflow-hidden flex items-center content-center'>
-        <img
-          src={characterImageUrl}
-          alt={name}
-          className='w-full absolute object-cover'
-        />
+        <InertiaLink
+          href={route('characters.view', id).toString()}
+          data-cy='view-character-link'
+        >
+          <img
+            src={characterImageUrl}
+            alt={name}
+            className='w-full absolute object-cover'
+          />
+        </InertiaLink>
       </div>
       <div className='card__details flex flex-col p-5 md:p-8 lg:p-10'>
         <h3 className='card__title text-3xl font-semibold'>
