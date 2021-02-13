bdd4b12e77310c7830a39e2726bff68506b62053
Alex Williams <alex@jump24.co.uk>
2021-02-13 17:00:40 +0000

    UPDATE: Further changes

--- ecs.php
+++ ecs.php
@@ -4,12 +4,16 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/vendor/jumptwentyfour/php-coding-standards/ecs.php');
-
     $parameters = $containerConfigurator->parameters();
-    
+
     $parameters->set(Option::PATHS, [
         __DIR__ . '/app',
         __DIR__ . '/tests',
     ]);
+
+    $parameters->set(Option::SKIP, [
+        'Unused parameter $request.' => null
+    ]);
+
+    $containerConfigurator->import(__DIR__ . '/vendor/jumptwentyfour/php-coding-standards/ecs.php');
 };
